c3a70f78aa4ac2be026506a2ee5b9956a6cc6bc5
Nick Garbalau <vonderklaas@proton.me>
2025-08-04 11:06:33 +0700

    feat: upload microservices

--- README.md
+++
@@ -1,5 +0,0 @@
-### Install Redis with Docker
-
-```cmd
-docker run -d --name redis-stack -p 6379:6379 -p 8001:8001 redis/redis-stack:latest
-```
--- utils/keys.ts
+++
@@ -1,8 +0,0 @@
-export function getKeyName(...args: string[]) {
-  return `modern-node:${args.join(":")}`;
-}
-
-export const restaurantKeyById = (id: string) => getKeyName("restaurants", id);
-export const reviewKeyById = (id: string) => getKeyName("reviews", id);
-export const reviewDetailsKeyById = (id: string) =>
-  getKeyName("reviews_details", id);
